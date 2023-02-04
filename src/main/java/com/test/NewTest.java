61dec47ddf5b25d41760fbe287ddc3f5900eb626
xhr1245769153 <1245769153@qq.com>
2023-02-04 21:23:50 +0800

    test1

--- src/main/java/com/test/NewTest.java
+++ src/main/java/com/test/NewTest.java
@@ -1,8 +1,13 @@
 package com.test;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**这是一个新增测试类
  * @author xhr
  * @date 21:21
  */
 public class NewTest {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
 }
